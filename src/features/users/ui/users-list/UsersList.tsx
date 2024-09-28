2bf69953929e04331c41d96108ec27f24ae1ee6e
BukreevSergey <bukreevwork2@gmail.com>
2024-09-28 14:21:12 +0300

    feat: add sort for user list table

--- src/features/users/ui/users-list/UsersList.tsx
+++ src/features/users/ui/users-list/UsersList.tsx
@@ -1,3 +1,4 @@
+import { InputMaybe, SortDirection } from '@/src/gql/graphql'
 import { useParamsHook } from '@/src/shared/hooks/useParamsHook'
 import { useQuery } from '@apollo/client'
 import { Input, Pagination, Select } from '@bitovyevolki/ui-kit-int'
@@ -12,17 +13,29 @@ export const UsersList = () => {
 
   const page = searchParams.get('page') ?? 1
   const pageSize = searchParams.get('pageSize') ?? 10
+  const sortBy = searchParams.get('sortBy') ?? 'userName'
+  const sortDirection = (searchParams.get('sortDirection') ?? 'asc') as InputMaybe<SortDirection>
 
   const { data, error, loading } = useQuery(GET_ALL_USERS, {
-    variables: { pageNumber: Number(page), pageSize: Number(pageSize) },
+    variables: { pageNumber: Number(page), pageSize: Number(pageSize), sortBy, sortDirection },
   })
+  const onSortChange = (column: string) => {
+    const newDirection = sortBy === column && sortDirection === 'asc' ? 'desc' : 'asc'
+
+    changeQueryHandler({
+      page: 1,
+      pageSize: pageSize,
+      sortBy: column,
+      sortDirection: newDirection,
+    })
+  }
 
   const onChangePageHandler = (page: number) => {
-    changeQueryHandler('page', page)
+    changeQueryHandler({ page: page })
   }
 
   const onChangePageSizeHandler = (pageSize: number) => {
-    changeQueryHandler('pageSize', pageSize)
+    changeQueryHandler({ pageSize: pageSize })
   }
 
   const totalCount = data?.getUsers.pagination.totalCount
@@ -33,7 +46,7 @@ export const UsersList = () => {
         <Input />
         <Select onChange={() => {}} options={[]} value={''} variant={'large'} />
       </div>
-      <UsersTable data={data} loading={loading} />
+      <UsersTable data={data} loading={loading} onSortChange={onSortChange} />
       {totalCount && (
         <Pagination
           onChangePage={onChangePageHandler}
